d8fe4f7cf77d7d39d8786ab665f8eecd9680af0a
Alejandro Vivas <ajvivas86@gmail.com>
2024-12-03 22:02:29 -0300

    feat: auto connect

--- src/modules/pos.js
+++ src/modules/pos.js
@@ -1,64 +1,65 @@
 import Transbank from "transbank-pos-sdk";
-import { POSIntegrado as FakePOS } from './fake-transbank-pos-sdk.js'
+import { POSIntegrado as FakePOS } from "./fake-transbank-pos-sdk.js";
 import { CONFIG } from "../config.js";
-import { Log } from './logger.js';
-import consola from 'consola';
+import { Log } from "./logger.js";
+import consola from "consola";
 
 let POS;
 // Pos Instance
 if (CONFIG.isDev) {
-    POS = new FakePOS();
+  POS = new FakePOS();
 } else {
-    POS = new Transbank.POSIntegrado();
-};
+  POS = new Transbank.POSIntegrado();
+}
 
 class TransbankSalesDetailException extends Error {
-    constructor(message) {
-      super(message);
-      this.name = "ValidationError"; 
-    }
+  constructor(message) {
+    super(message);
+    this.name = "ValidationError";
+  }
 }
-  
 
 // Dictionary
 const DICTIONARY = {
-    "0800": () => POS.loadKeys(), // TRANSACCIÓN CARGA DE LLAVES
-    "0100": () => POS.poll(), // TRANSACCIÓN DE POLLING
-    "0250": () => POS.getLastSale(), // TRANSACCIÓN ÚLTIMA VENTA
-    "0500": () => POS.closeDay(), // TRANSACCIÓN DE CIERRE
-    "0700": () => POS.getTotals(), // DETALLE DE VENTAS
-    "0300": () => POS.changeToNormalMode(), // CAMBIO DE MODALIDAD A POS NORMAL
-    "0260": (printOnPos) => {
-        consola.info(
-            `printOnPos: ${printOnPos}`
-        )
-        try {
-           
-           if (POS.waiting && printOnPos === '0') {
-            POS.ackCallback();
-           };
-            
-           return POS.salesDetail(printOnPos);
-        } catch (err) {
-            Log(err.message, 'salesDetail');
+  "0800": () => POS.loadKeys(), // TRANSACCIÓN CARGA DE LLAVES
+  "0100": () => POS.poll(), // TRANSACCIÓN DE POLLING
+  "0250": () => POS.getLastSale(), // TRANSACCIÓN ÚLTIMA VENTA
+  "0500": () => POS.closeDay(), // TRANSACCIÓN DE CIERRE
+  "0700": () => POS.getTotals(), // DETALLE DE VENTAS
+  "0300": () => POS.changeToNormalMode(), // CAMBIO DE MODALIDAD A POS NORMAL
+  "0260": (printOnPos) => {
+    consola.info(`printOnPos: ${printOnPos}`);
+    try {
+      if (POS.waiting) {
+        POS.waiting = false;
+      }
 
-            if (err instanceof TransbankSalesDetailException) {
-                consola.error('Ocurrió un error en el método [salesDetail] de tipo [TransbankSalesDetailException]: ', err.message);
-                return null
-            } else {
-                consola.error('Ocurrió un error en el método [salesDetail]', err.message);
-                return null;
-            }
-        }
-    },  // TRANSACCIÓN TOTALES
-    "0200": (amount, ticket, sendStatus, callback) => {
-        consola.info(
-            `amount: ${amount}, ticket: ${ticket}, sendStatus: ${sendStatus}, callback: ${callback}`
-        )
-        return POS.sale(amount, ticket, sendStatus, callback)
-    }, // TRANSACCIONES DE VENTA
-    "1200": (operationId) => POS.refund(operationId), // TRANSACCIÓN ANULACIÓN VENTA
+      return POS.salesDetail(printOnPos);
+    } catch (err) {
+      Log(err.message, "salesDetail");
 
+      if (err instanceof TransbankSalesDetailException) {
+        consola.error(
+          "Ocurrió un error en el método [salesDetail] de tipo [TransbankSalesDetailException]: ",
+          err.message
+        );
+        return null;
+      } else {
+        consola.error(
+          "Ocurrió un error en el método [salesDetail]",
+          err.message
+        );
+        return null;
+      }
+    }
+  }, // TRANSACCIÓN TOTALES
+  "0200": (amount, ticket, sendStatus, callback) => {
+    consola.info(
+      `amount: ${amount}, ticket: ${ticket}, sendStatus: ${sendStatus}, callback: ${callback}`
+    );
+    return POS.sale(amount, ticket, sendStatus, callback);
+  }, // TRANSACCIONES DE VENTA
+  1200: (operationId) => POS.refund(operationId), // TRANSACCIÓN ANULACIÓN VENTA
 };
 
 export { POS, DICTIONARY };
