96fcc73a5b4c2ef155b1a4ee6171e1bd6169ab97
Alejandro Vivas <ajvivas86@gmail.com>
2024-12-03 21:46:18 -0300

    feat: auto connect

--- src/index.js
+++ src/index.js
@@ -1,64 +1,57 @@
-import cron from 'node-cron';
-import consola from 'consola';
+import cron from "node-cron";
+import consola from "consola";
 import { CONFIG } from "./config.js";
-import { POS } from './modules/pos.js';
-import { checkRequest } from './modules/soap-client.js'
-import { downloadWSDL } from './modules/httpclient.js';
-import { Log } from './modules/logger.js';
+import { POS } from "./modules/pos.js";
+import { checkRequest } from "./modules/soap-client.js";
+import { downloadWSDL } from "./modules/httpclient.js";
+import { Log } from "./modules/logger.js";
 
-function startCron(keys) {
-  cron.schedule("*/5 * * * * *", () => {
-    consola.info("CRON ejecutado:", new Date().toISOString());
-    try {
-      checkRequest(keys);
-      } catch (err) {
-          console.log('startCron error', err.message);
-          Log(err.message, 'startCron');
-          return;
-      }
-  });
-};
-
-function startApp() {
-
+function startPOS() {
   if (CONFIG.isDev) {
-    consola.info('--------- RUNNING MOCKS ---------');
-  };
+    consola.info("--------- RUNNING MOCKS ---------");
+  }
 
   try {
 
-    if (!CONFIG.isDev)
-      downloadWSDL();
-
     POS.setDebug(true);
-    POS
-      .autoconnect()
+    POS.autoconnect()
       .then(async (port) => {
         if (port === false) {
           consola.error("No se encontró ningún POS conectado");
         } else {
-          consola.success("Connected to PORT:", port.path);
-          const keys = await POS.loadKeys();
-          const confirm = await consola.prompt("Quieres activar el cron?", {
-            type: "confirm",
-          });
-          
-          if(confirm) {
-            startCron(keys);
-            return;
-          } 
-          
-          checkRequest(keys);
+          consola.success("Conectado al Puerto:", port.path);
         }
       })
       .catch((err) => {
         consola.error(`Ocurrió un error inesperado. POS: ${err.message}`);
-        Log(err.message, 'autoconnect');
+        Log(err.message, "autoconnect");
       });
   } catch (err) {
     consola.error("Ocurrió un error al descargar archivo WSDL", err);
-    Log(err.message, 'startApp');
+    Log(err.message, "startApp");
   }
-};
+}
+
+function startApp() {
+  try {
+
+    if (!CONFIG.isDev) downloadWSDL();
+
+    cron.schedule("*/5 * * * * *", async () => {
+      consola.info("CRON ejecutado:", new Date().toISOString());
+      if (POS.isConnected()) {
+        const keys = await POS.loadKeys();
+        checkRequest(keys);
+        return;
+      }
+
+      startPOS();
+    });
+  } catch (err) {
+    console.log("startApp error", err.message);
+    Log(err.message, "startApp");
+    return;
+  }
+}
 
 startApp();
